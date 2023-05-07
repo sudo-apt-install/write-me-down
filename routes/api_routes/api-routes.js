5502949d19509781bbd5f8cf37f9a1f27c7e868b
Marq W <marquisewest86@duck.com>
2023-05-07 17:10:02 -0500

    adding all files

---
+++ routes/api_routes/api-routes.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const fs = require('fs');
+const uuid = require('uuid');
+const util = require('util');
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require("path");
+const routes = require("./routes/homeRoutes");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(routes);
+
+// GET Route for homepage
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
